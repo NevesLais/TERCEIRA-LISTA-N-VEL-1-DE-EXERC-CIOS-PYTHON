0702f795e618078c1c03d9c88bd5cd2744a29280
Laís de Souza Neves <116578290+NevesLais@users.noreply.github.com>
2025-05-22 20:59:27 -0300

    ex1.py

--- ex1.py
+++ ex1.py
@@ -1,1 +1,10 @@
+numeroInicial = int(input("Digite o número inicial: "))
+numeroFinal = int(input("Digite o numero final: "))
 
+tamanhoIntervalo = numeroFinal - numeroInicial
+
+if tamanhoIntervalo > 100:
+    print("Não pode ser maior do que 100")
+else:
+    for numeroInicial in range(numeroFinal):
+        print(str(numeroInicial))
