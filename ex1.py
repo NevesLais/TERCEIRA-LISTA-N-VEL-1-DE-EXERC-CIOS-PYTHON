27697affb603e9c85f2d8f9994cfdd5914cf456b
Laís de Souza Neves <116578290+NevesLais@users.noreply.github.com>
2025-05-29 21:21:24 -0300

    ex1.py

--- ex1.py
+++ ex1.py
@@ -1,4 +1,11 @@
-numeroInicial = int(input("Digite o número inicial: "))
+'''1 – Para treinar o laço de repetição FOR, crie um formulário e peça para o usuário digitar
+um número inicial e um número final, mostre o intervalo entre esses números, mas
+lembre-se que ele poderá digitar um intervalo muito grande que irá consumir muita
+memória do servidor, pensando nisso valide para que ele digite apenas intervalos de no
+máximo 100 números.'''
+
+
+numeroInicial = int(input("Digite o numero inicial: "))
 numeroFinal = int(input("Digite o numero final: "))
 
 tamanhoIntervalo = numeroFinal - numeroInicial
@@ -7,4 +14,4 @@ if tamanhoIntervalo > 100:
     print("Não pode ser maior do que 100")
 else:
     for i in range(numeroInicial + 1, numeroFinal):
-        print(str(i))
+        print(str(i)) 
